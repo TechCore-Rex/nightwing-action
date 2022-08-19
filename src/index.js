877463c136ef48ec312eb0b2717402564b22bc6c
Dylan Brookes <dylan.brookes10@gmail.com>
2022-08-19 15:48:34 -0500

    edit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import core from '@actions/core';
 
 async function handler() {
   core.info("TechCore deployment action initiated.");
-  const buildId = core.getInput("build-id");
+  const buildId = core.getInput("techcore-build-id");
   const techCoreToken = core.getInput("techcore-api-key");
   console.log(`your buildId: ${buildId}`);
   console.log(`your techCoreToken: ${techCoreToken}`);
