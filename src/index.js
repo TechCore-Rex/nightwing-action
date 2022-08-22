148c2e1d4eb1e7aed14dfda0116503ee03e8e669
Dylan Brookes <dylan.brookes10@gmail.com>
2022-08-21 19:59:50 -0500

    add error

--- src/index.js
+++ src/index.js
@@ -5,13 +5,10 @@ async function main() {
   core.info("TechCore build action initiated.");
   const buildId = core.getInput("techcore-build-id");
   const techCoreToken = core.getInput("techcore-api-key");
-  console.log('this is new');
-  console.log(`your buildId: ${buildId}`);
-  console.log(`your techCoreToken: ${techCoreToken}`);
 
   const {data: stream} = await axios.get("https://f779-216-106-133-65.ngrok.io", {
     responseType: "stream",
-  });
+  }).catch((e) => {throw new Error("Failed to get build stream!")});
 
   stream.on("data", (data) => {
     const parsedData = Buffer.from(data).toString("utf-8");
