143f420c7ae6c41fe71ef1429964e0e8b965ce96
Dylan Brookes <dylan.brookes10@gmail.com>
2022-08-21 19:01:17 -0500

    revert to js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import core from "@actions/core";
+import axios from 'axios';
+
+async function main() {
+  core.info("TechCore build action initiated.");
+  const buildId = core.getInput("techcore-build-id");
+  const techCoreToken = core.getInput("techcore-api-key");
+  console.log('this is new');
+  console.log(`your buildId: ${buildId}`);
+  console.log(`your techCoreToken: ${techCoreToken}`);
+
+  const {data: stream} = await axios.get("https://f779-216-106-133-65.ngrok.io", {
+    responseType: "stream",
+  });
+
+  stream.on("data", (data) => {
+    const parsedData = Buffer.from(data).toString("utf-8");
+    console.log(parsedData);
+  });
+
+  stream.on("end", () => {
+    console.log("stream done");
+  });
+}
+
+main();
