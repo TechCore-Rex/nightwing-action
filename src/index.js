b3f1440c8610734d5bf152dc1e08de2ea59c3b71
Dylan Brookes <dylan.brookes10@gmail.com>
2022-08-24 13:02:38 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ async function main() {
   core.info("TechCore build action initiated.");
   const buildId = core.getInput("techcore-build-id");
   const techCoreToken = core.getInput("techcore-api-key");
+  const logHost = core.getInput("techcore-api-url") ?? "https://api.techcore.com";
 
-  const {data: stream} = await axios.get("https://f779-216-106-133-65.ngrok.io", {
+  const url = `${logHost}/api/builds/${buildId}/logs`;
+  const {data: stream} = await axios.get(url, {
     responseType: "stream",
-  }).catch((e) => {throw new Error("Failed to get build stream!")});
+    timeout: 30_000,
+    headers: {
+      ['X-API-KEY']: techCoreToken,
+    },
+  }).catch(() => {throw new Error("Failed to get build stream!")});
 
   stream.on("data", (data) => {
     const parsedData = Buffer.from(data).toString("utf-8");
