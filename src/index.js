2ab7771994aa585bc8a670d5892f9805284027bf
Dylan Brookes <dylan.brookes10@gmail.com>
2022-08-25 09:32:09 -0500

    temp

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ async function main() {
   core.info("TechCore build action initiated.");
   const buildId = core.getInput("techcore-build-id");
   const techCoreToken = core.getInput("techcore-api-key");
-  const logHost = core.getInput("techcore-api-url") ?? "https://api.techcore.com";
+  // const logHost = core.getInput("techcore-api-url") ?? "https://api.techcore.com";
+  const logHost = "https://c2dc-66-90-165-82.ngrok.io";
 
   const url = `${logHost}/api/builds/${buildId}/logs`;
   const {data: stream} = await axios.get(url, {
